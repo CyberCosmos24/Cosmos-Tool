4af1274e76c8921b1f9396dbac6870eb7efb8e6a
Cosmos <email@cosmosportfolio.com>
2022-03-30 20:34:36 -0400

    Delete ping.py

--- NetworkTools/ping.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ip_list = ['1.1.1.1']
-for ip in ip_list:
-    response = os.popen(f"ping {ip}").read()
-    if "Received = 4" in response:
-        print(f" {ip}: HOST IS UP")
-    else:
-        print(f"{ip}: HOST IS DOWN")
